0c4c9cce55d57376f98ae5d4884f7997d03096c7
mesiyar <luokai@daishujiankang.com>
2021-01-04 17:22:25 +0800

    实现 http api 客户端

---
+++ httpClient.go
@@ -0,0 +1,61 @@
+// 分布式缓存需要实现节点间通信，建立基于 HTTP 的通信机制是比较常见和简单的做法。
+// 如果一个节点启动了 HTTP 服务，那么这个节点就可以被其他节点访问。
+package ruoCache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultBasePath = "/api/"
+
+type HttpPool struct {
+	// 当前节点的url地址, e.g. "https://example.net:8000"
+	self     string
+	basePath string
+}
+
+func NewHttpPool(self string) *HttpPool {
+	return &HttpPool{
+		self:     self,
+		basePath: defaultBasePath,
+	}
+}
+
+func (p *HttpPool) Log(format string, v ...interface{}) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+
+func (p *HttpPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HttpPool serving unexpected path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+	// /<basepath>/<groupname>/<key> required
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
+}
